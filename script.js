05b36639df9fe6ebcc5cf1023ed506f43bfd21a2
BLESSY M K <blessyarun2007@gmail.com>
2026-06-23 22:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function login() {
-  const user = document.getElementById("username").value;
-  if (user) {
-    localStorage.setItem("user", user);
-    window.location.href = "index.html";
-  }
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function addTask() {
+  const task = document.getElementById("task").value;
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTasks();
 }
+
+function showTasks() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+  tasks.forEach((t, i) => {
+    list.innerHTML += `<li>${t}
+      <button onclick="deleteTask(${i})">❌</button>
+    </li>`;
+  });
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  showTasks();
+}
+
+function logout() {
+  localStorage.removeItem("user");
+  window.location.href = "login.html";
+}
+
+showTasks();
